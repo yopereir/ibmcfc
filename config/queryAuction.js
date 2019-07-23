425d86f756355da15428be39c450884a4931356f
unknown <yohann.pereira28@gmail.com>
2019-07-23 13:01:11 -0400

    testing auction

--- config/queryAuction.js
+++ config/queryAuction.js
@@ -30,13 +30,15 @@ async function main() {
 
     // Submit the 'createCar' transaction to the smart contract, and wait for it
     // to be committed to the ledger.
+    if(await contract.submitTransaction('doesAuctionExist','1')) await contract.submitTransaction('closeAuction','1');
     let result = await contract.submitTransaction('initiateAuction','1','13210','13220');
     console.log(result.toString());
     await contract.submitTransaction('bid','1','1000','Bob','666');
     await contract.submitTransaction('bid','1','2000','Alice','667');
     await contract.submitTransaction('bid','1','500','Duke','668');
     await contract.submitTransaction('bid','1','1500','Jim','669');
-    await contract.submitTransaction('getAuctionInfo','1','1000','Bob','666');
+    result = await contract.submitTransaction('getAuctionInfo','1');
+    console.log(result.toString());
 
     await gateway.disconnect();
 
